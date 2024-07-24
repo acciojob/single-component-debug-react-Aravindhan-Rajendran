ce8c6d997cdf5d3c42f4b637966bed06d0d79230
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-24 16:07:49 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-<p id="root>ReactDOM.render</p>
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
